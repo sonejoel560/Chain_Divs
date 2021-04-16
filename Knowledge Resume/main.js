8aa4d34b8d0c7c739d1f1f651d2b3daaab20280b
sonejoel560 <67550701+sonejoel560@users.noreply.github.com>
2021-04-16 22:20:49 +0200

    Add files via upload

---
+++ Knowledge Resume/main.js
@@ -0,0 +1,29 @@
+conts items = document.querySelectorAll
+('#timeline li');
+
+cont isInViewport = el => {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= 
+          (window.innerHeight ||
+          document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth ||
+        document.documentElement.clientWidth)
+    );
+};
+
+const run = () =>
+  items.forEach(item => {
+      if (isInViewport(item)) {
+          item.classList.add('show');
+      }
+  });
+
+//   Events
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
+
+// My javascript implementation didn't work out, will figure it out later when I must've studied it
